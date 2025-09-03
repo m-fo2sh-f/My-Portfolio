68ce5b62e97d561a5e0ddbda18f90f4920930201
Mohamed Fouad <mohamedabc336@gmail.com>
2025-09-04 01:17:13 +0300

    update project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/My-Portfolio/", // 👈 ده المسار اللي هيخلي الصور وملفات js/css تشتغل صح
 })
